bb83aafa8a4d395101e4ea601e09ad7aa37ebde2
anjithkj <127194209+anjithkj@users.noreply.github.com>
2023-09-27 03:03:12 -0700

    readfirst
    all instruction are add read first

---
+++ Readme.jsx
@@ -0,0 +1,10 @@
+sign up in firebase
+click on get started
+add projects or existing projects
+enetr project name
+off the google Analyser
+create project
+
+in existing project
+build
+realtime database
